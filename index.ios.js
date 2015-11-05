f73ace993f7161a3d73bec96f2cb6838667fa8d2
Stan Bershadskiy <stan@moduscreate.com>
2015-11-05 00:54:44 -0500

    parse data

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,54 @@
  */
 'use strict';
 
-var React = require('react-native');
+var React    = require('react-native'),
+    Schedule = require('./js/Schedule');
+
+new Schedule().loadSchedule();
+
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+        AppRegistry,
+        StyleSheet,
+        Text,
+        View,
+        } = React;
 
 var BoxSched = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render : function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container    : {
+        flex            : 1,
+        justifyContent  : 'center',
+        alignItems      : 'center',
+        backgroundColor : '#F5FCFF',
+    },
+    welcome      : {
+        fontSize  : 20,
+        textAlign : 'center',
+        margin    : 10,
+    },
+    instructions : {
+        textAlign    : 'center',
+        color        : '#333333',
+        marginBottom : 5,
+    },
 });
 
 AppRegistry.registerComponent('BoxSched', () => BoxSched);
---
+++ js/Schedule.js
@@ -0,0 +1,113 @@
+/**
+ * Created by stan229 on 11/3/15.
+ */
+
+var cheerio = require('cheerio');
+
+var SCHED_URL = 'http://espn.go.com/boxing/story/_/id/12508267/boxing-fight-schedule';
+
+class Schedule {
+    loadSchedule() {
+        fetch(SCHED_URL).then((response) => response.text()).then(responseData => this.parseContent(responseData)).done();
+    }
+
+    parseContent(responseData) {
+        var $            = cheerio.load(responseData),
+            children     = $('.article-body').children(),
+            length       = children.length,
+            scheduleData = [],
+            monthData,
+            event,
+            childNode,
+            childNodeText,
+            childNodeChildren,
+            childNodeRawChildrenLength,
+            i;
+
+        for (i = 0; i < length; i++) {
+            childNode = $(children[i]);
+
+            if (childNode[0].name === 'h2') {
+                monthData && scheduleData.push(monthData);
+                event && monthData.events.push(event);
+
+                event = null;
+
+                monthData = {
+                    month  : childNode.text(),
+                    events : []
+                };
+            }
+
+            if (childNode[0].name === 'p') {
+                childNodeChildren = childNode.children();
+                childNodeRawChildrenLength = childNode[0].children.length;
+
+                if (childNodeRawChildrenLength === 1) {
+                    event && monthData.events.push(event);
+                    event = {
+                        date  : $(childNodeChildren[0]).text(),
+                        cards : []
+                    };
+                }
+
+                if (childNodeRawChildrenLength > 1) {
+                    childNodeText = childNode.text();
+                    event.cards.push(this.parseCard(childNodeText, childNodeChildren));
+                }
+
+
+            }
+        }
+        monthData.events.push(event);
+        scheduleData.push(monthData);
+
+        return scheduleData;
+    }
+
+    parseCard(text, childNodeChildren) {
+        var location       = text.substring(0, text.indexOf(':')),
+            openParenIndex = location.indexOf('('),
+            channel        = openParenIndex > -1 && location.substring(openParenIndex + 1, location.length - 1),
+            location       = location.substr(0, openParenIndex - 1),
+            fightsText     = text.substr(location.length + 2),
+            fightsSplit    = fightsText.split('; '),
+            fightsLength   = fightsSplit.length,
+            fights         = [],
+            fight,
+            fightText,
+            i;
+
+        for (i = 0; i < fightsLength; i++) {
+            fightText = fightsSplit[i];
+
+            fight = {
+                text       : fightText,
+                titleFight : this.isChampionFight(fightText, childNodeChildren)
+            }
+            fights.push(fight);
+        }
+
+        return {
+            location : location,
+            channel  : channel,
+            fights   : fights
+        }
+    }
+
+    isChampionFight(fightText, childNodeChildren) {
+        var length = childNodeChildren.length,
+            i;
+
+        for (i = 0; i < length; i++) {
+            if (childNodeChildren[i].children[0].data === fightText) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
+
+module.exports = Schedule;
